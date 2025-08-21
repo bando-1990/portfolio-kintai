60b60959e09d6c3cd70d348cb6fe20f2f3b9199e
DaikiBando <bando.1990.may.1342@gmail.com>
2025-08-22 07:41:49 +0900

    てすと

--- kintai-app/src/main/java/com/example/kintai/KintaiApplication.java
+++ kintai-app/src/main/java/com/example/kintai/KintaiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.kintai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class KintaiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KintaiApplication.class, args);
 	}
-
+//テスト
 }
